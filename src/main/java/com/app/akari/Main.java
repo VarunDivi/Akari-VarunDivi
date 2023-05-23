567005015ac30385a9ff5b0d397e7ab58d1fc954
Varun Divi <vdivi33@ad.unc.edul>
2023-05-22 21:40:07 -0400

    Did it work?

--- src/main/java/com/app/akari/Main.java
+++ src/main/java/com/app/akari/Main.java
@@ -7,5 +7,6 @@ import javafx.application.Application;
 public class Main {
   public static void main(String[] args) {
     Application.launch(AppLauncher.class);
+    System.out.println("Did it work?");
   }
 }
